353158bf61df88463e10b83c2f2bff936b832bc6
JOSHUAPUDI <102248971+Brgysibulan@users.noreply.github.com>
2026-09-04 09:59:36 +0800

    Add app navigation model

---
+++ app/src/main/java/ph/gov/barangaysibulan/idmaker/ui/AppScreen.kt
@@ -0,0 +1,8 @@
+package ph.gov.barangaysibulan.idmaker.ui
+
+enum class AppScreen(val title: String) {
+    HOME("Home"),
+    RECORDS("Employee Records"),
+    GENERATE("Generate ID"),
+    SETTINGS("Settings")
+}
